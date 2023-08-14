cf72c3868e79dbbdb46c373b9d4b89d49f5d2abe
yatin73 <60352795+yatin73@users.noreply.github.com>
2023-08-14 21:20:49 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,9 +1,9 @@
 export default {
-  name: "Yatin", // actual name of the recipient (Mandatory)
+  name: process.env.NAME, // actual name of the recipient (Mandatory)
 
   nickname: process.env.NICKNAME, // nickname(optional)
 
-  pic: "sample-pic.jpeg", // image url of recipients (Mandatory)
+  pic: process.env.PIC, // image url of recipients (Mandatory)
 
   showScrollMsg: process.env.SCROLL_MSG, // set to false if you do not want the scrolling message
 
